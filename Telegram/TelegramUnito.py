5cdb3cfaecfd0749ba1002a8b1329bc71c8d4aab
LulaSan <83768258+LulaSan@users.noreply.github.com>
2021-10-20 11:28:12 +0200

    Update TelegramUnito.py

--- Telegram/TelegramUnito.py
+++ Telegram/TelegramUnito.py
@@ -756,7 +756,7 @@ def NewThreshold_humidity_reply(update: Update, context: CallbackContext) -> int
           modified_dict={"THRESHOLD_MOIST_MIN": int(text[2])}
           requests.post(url=f"{SERVER}/plant/{plantid}",json=modified_dict)
     lista=listaCROPS(farmerid)
-    update.message.reply_text(text=f"Ecco la lista aggiornata\n {lista}\n puoi continuare oppure scrivere 'principale' per tornare al menu principale )
+    update.message.reply_text(text=f"Ecco la lista aggiornata\n {lista}\n puoi continuare oppure scrivere 'principale' per tornare al menu principale")
     return FARMER_TYPING_2
 
   elif text[1]=="max":
