08632487177de87d0372b7e6a5712e4b084fd156
LulaSan <83768258+LulaSan@users.noreply.github.com>
2021-10-26 17:32:57 +0200

    Update Catalog_REST.py

--- Catalog/Catalog_REST.py
+++ Catalog/Catalog_REST.py
@@ -133,9 +133,9 @@ if __name__=="__main__":
     cherrypy.config.update({'server.socket_host': '0.0.0.0'})
     cherrypy.config.update({'server.socket_port': 2000})
     
-    cherrypy.engine.exit()
-    #cherrypy.quickstart(CHERRY_CATALOG(),'/',conf)
+    #cherrypy.engine.exit()
+    cherrypy.quickstart(CHERRY_CATALOG(),'/',conf)
 
-   #cherrypy.engine.block()
+    #cherrypy.engine.block()
 
     #cherrypy.engine.block()
