6b9d9ca20c80b6801af439ee45b69d95b6e4f47b
gregoriobattaglia <74959904+gregoriobattaglia@users.noreply.github.com>
2021-11-14 01:50:27 +0100

    Add files via upload

---
+++ 0_Sensors_&_Controllers/Green/Hum/2_Green_Hum.py
@@ -0,0 +1,48 @@
+from MyMQTT import*
+from Emulated_sensors import Sensor_b, Sensor_h, TB_Sensor_b, TB_Sensor_h
+# import paho.mqtt.client as mqtt
+import time
+import random
+import numpy as np
+import requests
+
+# ABCDEFG
+if __name__=="__main__":
+
+    # Simulated Value
+    time_alive = 24*60*1000
+    lam = 24
+    val = 45+0.3*np.random.poisson(lam, time_alive) 
+    val = np.clip(val, 10, 60)
+
+    x = np.arange(0, 3 * np.pi, 0.000005)
+    batt = 40 + 20* np.sin(x)
+    batt = np.round(batt, 2)
+    batt = np.clip(batt, 0, 100)
+
+    # Configuration for MQTT
+
+    # conf=json.load(open("settings.json"))
+    id="ABCDEFG"    
+
+    S_cat=Sensor_h(id,"/p4iot/greenhouses/{}/sensors/brigthness".format(id),"13.59.136.106", 1883)
+    S_tb = TB_Sensor_h(id,'GqJXcA2EtMZfPzs1FvaF','demo.thingsboard.io','v1/devices/me/telemetry',1883,60)
+    
+    S_cat.run()    
+    S_tb.con()
+
+    for cnt in range(len(val)): 
+
+        greenHouseInfo = json.loads(requests.get(
+            'http://ec2-13-59-136-106.us-east-2.compute.amazonaws.com:2000/greenhouses'#/ABCDEFG/COMMANDS_TOPIC'
+            ).text)
+        #THRESHOLD_BRIGHT
+        status = greenHouseInfo[["GREENHOUSE_ID"]==id]["STATUS_CONTROLL_HUMIDITY"]
+
+        sensor_data = {"humidity" : val[cnt], "battery": batt[cnt] ,"status": status}  
+        S_cat.sendData(val[cnt],batt[cnt],status)
+        S_tb.pub_tb(sensor_data)
+        time.sleep(1)
+
+    S_cat.end()
+    S_tb.unconn()
---
+++ 0_Sensors_&_Controllers/Plant/Pomodoro/Pomodoro_Hum_Temp.py
@@ -0,0 +1,52 @@
+from MyMQTT import*
+from Emulated_sensors import *
+# import paho.mqtt.client as mqtt
+import time
+import random
+import numpy as np
+import requests
+
+
+if __name__=="__main__":
+
+    # Simulated val_hue
+    time_alive = 24*60*1000
+    lam = 24
+    val_h = 20+0.7*np.random.poisson(lam, time_alive) 
+    val_h = np.clip(val_h, 10, 60)
+    
+    val_t = 25+0.3*np.random.poisson(lam, time_alive) 
+    val_t = np.clip(val_t, 10, 60)
+
+    x = np.arange(0, 3 * np.pi, 0.005)
+    batt = 40 + 20* np.sin(x)
+    batt = np.clip(batt, 0, 100)
+
+    # Configuration for MQTT
+
+    # conf=json.load(open("settings.json"))
+    id="84F3EB33DB49"
+    
+    S_db=Sensor_plant(id,"/p4iot/plants/{}/sensors".format(id),"13.59.136.106", 1883)
+    S_tb = TB_Sensor_plant(id,'FgbOm7jGYMFUENoeISR0','demo.thingsboard.io','v1/devices/me/telemetry',1883,60)
+    print("Subscriber connected to: /p4iot/plants/{}/sensors".format(id))
+
+
+    S_db.run()      
+    S_tb.con()
+    
+    for cnt in range(len(val_h)):
+        plantInfo = json.loads(requests.get(
+        'http://ec2-13-59-136-106.us-east-2.compute.amazonaws.com:2000/plants'
+        ).text)
+        
+        status_h = plantInfo[["PLANT_ID"]==id]["STATUS_PUMP"]
+        
+        sensor_data = {"humidity" : val_h[cnt], "temperature": val_t[cnt], "battery": batt[cnt] ,"status_pump": status_h}  
+        S_db.sendData(val_h[cnt],val_t[cnt],batt[cnt],status=status_h,binar=id)
+        S_tb.pub_tb(sensor_data)
+        time.sleep(1)
+
+    S_db.end()
+
+    S_tb.unconn()
