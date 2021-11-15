f57dfbc0ab5d9e7cc9bee66600170045003a47a1
LulaSan <83768258+LulaSan@users.noreply.github.com>
2021-11-15 11:15:01 +0100

    Update Telegram_Bot.py

--- Telegram/Telegram_Bot.py
+++ Telegram/Telegram_Bot.py
@@ -673,32 +673,38 @@ def pompaonoff(update: Update, context: CallbackContext) -> int:
     text = update.message.text.split(" ")
     plant_pump=text[0]
     plants=json.loads(requests.get(url=f"{SERVER}/plants").text)
-    for plant in plants:
-        if (re.search(plant["PLANT_NAME"], plant_pump, re.IGNORECASE))!=None:
-            if plant["OWNER"]==farmerid:
-                if plant["STATUS_PUMP"]== 1:
-                    json_mod={"STATUS_PUMP": 0}
-                    r=requests.post(url=SERVER+f"/plant/{plantid}",json=json_mod)
-                    newlist=listpumps(farmerid)
-                    update.message.reply_text(f"Pump of {plant_pump} is now OFF.\n" 
-                                              f"\n Here there is the updated list:\n {newlist} \n "
-                                              "\n Try again in you want to modify more. \n"
-                                              "\n Write 'Principale' to go back to the main menu. \n")
-                    return FARMER_TYPING_3
-                else:
-                    json_mod={"STATUS_PUMP": 1}
-                    update.message.reply_text(f"Pump of {plant_pump} is now ON \n Write 'Principale' to go back to the main menu")
-                    r=requests.post(url=SERVER+f"/plant/{plantid}",json=json_mod)
-                    newlist=listpumps(farmerid)
-                    update.message.reply_text(f"Pump of {plant_pump} is now OFF.\n" 
-                                              f"\n Here there is the updated list:\n {newlist} \n "
-                                              "\n Try again in you want to modify more. \n"
-                                              "\n Write 'Principale' to go back to the main menu. \n")
-                    return FARMER_TYPING_3
-               
-        else:
-            update.message.reply_text(f" This plant doesn't exist! \n Try again, please.")
-            return FARMER_TYPING_3
+    if text[0] == 'Principale' :
+        update.message.reply_text('Scegli tra:', reply_markup=reply_markupPrincipale_FARMER)
+        return FARMER
+    else:
+        for plant in plants:
+            print(f"{plant["PLANT_NAME"]}, {plant_pump}")
+            if (re.search(plant["PLANT_NAME"], plant_pump, re.IGNORECASE))!=None:
+
+                if plant["OWNER"]==farmerid:
+                    if plant["STATUS_PUMP"]== 1:
+                        json_mod={"STATUS_PUMP": 0}
+                        r=requests.post(url=SERVER+f"/plant/{plantid}",json=json_mod)
+                        newlist=listpumps(farmerid)
+                        update.message.reply_text(f"Pump of {plant_pump} is now OFF.\n" 
+                                                  f"\n Here there is the updated list:\n {newlist} \n "
+                                                  "\n Try again in you want to modify more. \n"
+                                                  "\n Write 'Principale' to go back to the main menu. \n")
+                        return FARMER_TYPING_3
+                    else:
+                        json_mod={"STATUS_PUMP": 1}
+                        update.message.reply_text(f"Pump of {plant_pump} is now ON \n Write 'Principale' to go back to the main menu")
+                        r=requests.post(url=SERVER+f"/plant/{plantid}",json=json_mod)
+                        newlist=listpumps(farmerid)
+                        update.message.reply_text(f"Pump of {plant_pump} is now OFF.\n" 
+                                                  f"\n Here there is the updated list:\n {newlist} \n "
+                                                  "\n Try again in you want to modify more. \n"
+                                                  "\n Write 'Principale' to go back to the main menu. \n")
+                        return FARMER_TYPING_3
+
+            else:
+                update.message.reply_text(f" This plant doesn't exist! \n Try again, please.")
+                return FARMER_TYPING_3
 
 def listaCROPS(farmerid):
   farmer=json.loads(requests.get(url=f"{SERVER}/farmer/{farmerid}").text)
