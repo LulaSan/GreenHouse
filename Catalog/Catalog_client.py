289a682724ab16d140bd49a210119dae1614f188
LulaSan <83768258+LulaSan@users.noreply.github.com>
2021-11-16 12:42:16 +0100

    Update Catalog_client.py

--- Catalog/Catalog_client.py
+++ Catalog/Catalog_client.py
@@ -97,7 +97,9 @@ class PlantClient():
                 #removing the crop also from the list of crops owned by the farmer
                 owner= self.PlantsList[i]["OWNER"]
                 farmer=json.loads(requests.get(SERVER+f"/farmer/{owner}").text)
+                print(farmer)
                 cropsowned=farmer["CROPS_OWNED"]
+                print(cropsowned)
                 cropsmod=cropsowned.remove(deviceID)
                 json_mod={"CROPS_OWNED" : cropsmod }
                 r=requests.post(SERVER+f"/uporaddfarmer/{owner}",json=json_mod)
